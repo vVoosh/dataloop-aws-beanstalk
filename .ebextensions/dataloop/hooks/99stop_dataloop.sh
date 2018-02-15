c4fd5468dba1426fa38d88bf54112b111eee1a9f
Felipe Lopes <31849154+felipe-vvoosh@users.noreply.github.com>
2018-02-15 17:15:34 +0000

    Rollback

--- .ebextensions/dataloop/hooks/99stop_dataloop.sh
+++ .ebextensions/dataloop/hooks/99stop_dataloop.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 # .ebextensions/dataloop/hooks/99stop_dataloop.sh
-/etc/init.d/dataloop-agent status
-RESULT=$?
-if [ $RESULT != 3 ]; then
-  /etc/init.d/dataloop-agent stop
+if [ -e /etc/init.d/dataloop-agent ]; then
+  /etc/init.d/dataloop-agent stop || true
 fi
